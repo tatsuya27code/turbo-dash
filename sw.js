237e12078b74932f7f0be6d4d0e0f9147bd27f30
tatsuya27code <tatsuya.o.mtb@gmail.com>
2026-05-01 23:47:43 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
 // 冒頭に追加
-let gameState = 'TITLE'; 
+let gameState = 'TITLE';
+let isPaused = false; // 一時停止中かどうかを覚える変数
+
+document.getElementById('stop-button').addEventListener('click', () => {
+    isPaused = !isPaused; // 押すたびに「停止/再開」を切り替える
+    
+    if (isPaused) {
+        document.getElementById('stop-button').innerText = 'RESUME'; // 再開ボタンに文字を変える
+    } else {
+        document.getElementById('stop-button').innerText = 'STOP';
+    }
+});
+
 
 // ボタンクリックでゲーム開始
 document.getElementById('start-button').addEventListener('click', () => {
